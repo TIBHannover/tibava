83f03210c9eacc3e88a8ad738d5dac0a900be0b6
matthias springstein <matthias.springstein@gmail.com>
2026-06-30 20:53:43 +0200

    Add a whitelist and a blacklist parameter for which plugin is available in the backend.

--- backend/src/backend/backend/views/plugin.py
+++ backend/src/backend/backend/views/plugin.py
@@ -20,26 +20,9 @@ class PluginList(View):
     def get(self, request):
         plugin_manager = PluginManager()
         try:
-            video_id = request.GET.get("video_id")
-            if video_id:
-                video_db = Video.objects.get(id=video_id)
-                analyses = PluginRun.objects.filter(video=video_db)
-            else:
-                analyses = PluginRun.objects.all()
-
-            add_results = request.GET.get("add_results")
-            if add_results:
-                entries = []
-                for x in analyses:
-                    results = plugin_manager.get_results(x)
-                    if results:
-                        entries.append({**x.to_dict(), "results": results})
-                    else:
-                        entries.append({**x.to_dict()})
-            else:
-                entries = [x.to_dict() for x in analyses]
+            entries = [{"name": x} for x in plugin_manager.plugins()]
 
             return JsonResponse({"status": "ok", "entries": entries})
         except Exception:
-            logger.exception('Failed to list plugins')
+            logger.exception("Failed to list plugins")
             return JsonResponse({"status": "error"})
